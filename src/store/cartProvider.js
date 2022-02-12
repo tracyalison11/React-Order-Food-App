7fc359b19e55be017ee727dc10a6c2a9aea32777
Tracy King <tracyak13@gmail.com>
2022-02-12 01:14:24 -0500

    Implementing Cart Context

--- src/store/cartProvider.js
+++ src/store/cartProvider.js
@@ -1,8 +1,19 @@
 import CartContext from './cart-context';
 
 const CartProvider = props => {
+  const addItemToCartHandler = item => {};
+
+  const removeItemFromCartHandler = id => {};
+
+  const cartContext = {
+    items: [],
+    totalAmount: 0,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromCartHandler
+  };
+
   return (
-    <CartContext.Provider>
+    <CartContext.Provider value={cartContext}>
       {props.children}
     </CartContext.Provider>
   );
