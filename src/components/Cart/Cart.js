6a9527256221477b06fc9486744fe80ab6eaa35d
Tracy King <tracyak13@gmail.com>
2022-02-12 02:30:51 -0500

    Updates to add/remove item functionality from cart modal

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -7,15 +7,15 @@ import CartContext from '../../store/cart-context';
 const Cart = (props) => {
   const cartCtx = useContext(CartContext);
 
-  const totalAmount = `$$cartCtx.totalAmount.toFixed(2)`;
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
   const cartItemRemoveHandler = id => {
-
+    cartCtx.removeItem(id);
   };
 
   const cartItemAddHandler = item => {
-
+    cartCtx.addItem({...item, amount: 1});
   };
 
   const cartItems = (
@@ -26,7 +26,7 @@ const Cart = (props) => {
           name={item.name}
           price={item.price}
           onRemove={cartItemRemoveHandler.bind(null, item.id)}
-          onAddProp={cartItemAddHandler.bind(null, item)}
+          onAdd={cartItemAddHandler.bind(null, item)}
         />
       ))}
     </ul>
