9cf809c161ddc24fc9a9263c15d26114b320b83e
Tracy King <tracyak13@gmail.com>
2022-02-12 01:47:54 -0500

    Cart logic updates

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,23 @@
+import { useContext } from 'react';
 import classes from './HeaderCartButton.module.css';
 import CartIcon from '../Cart/CartIcon';
 
+import CartContext from "../../store/cart-context";
+
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItems = cartCtx.items.reduce((currentNumber, item) => {
+    return currentNumber + item.amount;
+  }, 0)
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
       <span></span>
     </button>
   );
--- src/store/cartProvider.js
+++ src/store/cartProvider.js
@@ -1,13 +1,37 @@
+import { useReducer } from 'react';
 import CartContext from './cart-context';
 
-const CartProvider = props => {
-  const addItemToCartHandler = item => {};
+const defaultCartState = {
+  items: [],
+  totalAmount: 0
+};
 
-  const removeItemFromCartHandler = id => {};
+const cartReducer = (state, action) => {
+  if (action.type === 'ADD_CART_ITEM') {
+    const updatedItems = state.items.concat(action.item);
+    const updatedTotalAmount = state.totalAmount = action.item.price * action.item.amount;
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount
+    };
+  }
+  return defaultCartState;
+};
+
+const CartProvider = props => {
+  const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
+
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({type: 'ADD_CART_ITEM', item: item});
+  };
+
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({type: 'REMOVE_CART_ITEM', id: id});
+  };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler
   };
